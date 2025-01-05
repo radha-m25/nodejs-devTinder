3af4ef2fa87bd933c2a9c010b5f037c79bd2dfd6
Radha M <radha.m@tavant.com>
2025-01-05 21:46:37 +0530

    dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,50 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home",(req,res) => {
-    res.send("From home");
+//http://localhost:7777/abcd
+//http://localhost:7777/abcccccccccd
+app.get("/abc+d",(req,res) => {
+    res.send("abcd, abcccc..d. start with ab and ends with d, with c in between count as you needed");
 })
 
-app.use("/productOne",(req,res) => {
-    res.send("From productOne");
+// http://localhost:7777/abcbcbcd
+// anything you can write b/w ab and d. but start with ab and end with d
+// "ab(cd)?f" - here bc is optional
+app.get("/abc*d",(req,res) => {
+    res.send("abcd, abcbcbc..d. start with ab and ends with d, with bc in between count as you needed");
 })
 
-app.use("/productTwo",(req,res) => {
-    res.send("From productTwo");
+
+app.get("/ab?c",(req,res) => {
+    res.send("u can add anything after abc");
+})
+
+//.*fly$/ - start with anything but end with fly
+app.get(/.*fly$/,(req,res) => {
+    res.send("start with anything but end with fly");
+})
+
+app.get("/dashboard/:username/:password", (req,res) => {
+    console.log(req.params);
+    res.send("Welcome to the dashboard");
+})
+
+// http://localhost:7777/signup?username=user1
+app.get("/signup",(req,res) => {
+    console.log(req.query);
+    res.send("Sign up page");
+})
+
+app.post("/home", (req,res) => {
+    res.send("Welcome to the home page, post call done");
+})
+
+app.delete("/product",(req,res) => {
+    res.send("Product deleted");
+})
+
+app.put("/login",(req,res) => {
+    res.send("login updated");
 })
 
 app.listen(7777, () => {
--- steps.md
+++ steps.md
@@ -11,3 +11,26 @@
 npm install -g nodemon
 11. update command in package.json (under script)
 12. diff b/w caret and tilde (^ vs ~)
+
+- intialize git 
+- gitignore
+- create a remote repo on git
+- push all the code to remote origin
+- play with routes and route extensions ex. /hello,/hello/2,/xyz
+- order of the routes matter a lot
+- install postman app and make a workspace/collection > test API call
+- write logic to handle GET,POST,PATCH,DELETE API calls and test them on postman
+- explore routing and use of ?,+,*,() in the routes
+- use of regex in routes /a/, /.*fly$/
+- dynamic routes (request body and request params)
+
+- Multiple route handlers - Play with the code
+- next()
+- next function and errors along with res.send()
+- app.use("/route",rH,[rH2,rH3],rH4,rH5);
+- what is a middleware? why do we need it?
+- How express JS basically handles requests behind the scenes
+- diff app.use and app.all
+- Write a dummy auth middleware for admin
+- write a dummy auth middleware for all except /user.login
+- error handling using a app.use("/) - (err,req,res,next)
